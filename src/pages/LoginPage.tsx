980dc067db9229d260041a7e51181d6261bc77e7
Sergiu13372 <sergiudarie1337@gmail.com>
2022-05-16 00:24:08 +0300

    Test deploy

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -41,7 +41,7 @@ function LoginPage() {
     data.push(response['accessToken'])
     data.push(response['profileObj'])
     console.log(data)
-    fetch("http://127.0.0.1:8000/login", {
+    fetch("http://middleware-hackai-bakcend.azurewebsites.net/login", {
       method: "POST",
       headers: {'Content-Type': 'application/json'}, 
       body: JSON.stringify(data)
