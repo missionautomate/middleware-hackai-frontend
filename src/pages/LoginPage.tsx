01a410f4288e45af27870dc282427f0643c2e78e
CopoeruDavid <copoerudavid25@gmail.com>
2022-06-04 22:12:26 +0200

    added microsoft login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -11,6 +11,9 @@ import UserContext from '../context/User.context';
 import { UserBasics } from '../models/user-basics.data';
 import { UserRole } from '../components/enums/user-role';
 import {useGoogleOneTapLogin} from 'react-google-one-tap-login';
+import googleOneTap from 'google-one-tap';
+import GoogleOneTapLogin from './../components/GoogleOneTap'
+import MicrosoftLogin from "react-microsoft-login";
 
 class PictureData {
   name: string = '';
@@ -23,11 +26,12 @@ const revDb: PictureData[] = [];
 let revIndex = 0;
 
 function LoginPage() {
+
   const navigate = useNavigate();
   const {user, changeUser} = useContext(UserContext);
 
   const [isLoading, setLoading] = useState(true);
-  const [cookies, setCookie, removeCookie] = useCookies(['google-token', 'google-id']);
+  const [cookies, setCookie, removeCookie] = useCookies(['google-token', 'unique-id']);
   
   useEffect(() => {
     if ('google-token' in cookies){
@@ -40,19 +44,20 @@ function LoginPage() {
     <ReactLoading type={'bars'} color={'#ffffff'} height={667} width={375} />
   );
   
-  const responseGoogle = (response: any) => {
-    console.log(response);
+  const responseGoogle = (err:any, response:any) => {
+    console.log(err, response);
     const data = []
-    data.push(response['accessToken'])
-    data.push(response['profileObj'])
+    data.push(response['account']['accountIdentifier'])
+    data.push(response['account']['name'])
+    data.push(response['account']['userName'])
     console.log(data)
-    setCookie("google-token", response.tokenObj.id_token, {expires: new Date(response.tokenObj.expires_at), maxAge: response.tokenObj.expires_in});
-    setCookie("google-id", response['profileObj'].googleId)
+    setCookie("google-token", response['accessToken'], {expires: response['expiresOn']});
+    setCookie("unique-id", response['account']['accountIdentifier'])
     const newUser = new UserBasics();
     newUser.userRole = UserRole.LOGED_IN_USER;
-    newUser.name = response['profileObj'].name;
-    newUser.email = response['profileObj'].email;
-    newUser.pictureURL = response['profileObj'].imageUrl;
+    newUser.name = response['account']['name'];
+    newUser.email = response['account']['userName'];
+    newUser.pictureURL = "https://upload.wikimedia.org/wikipedia/commons/d/d6/Connecticut_ComiCONN_Superhero_Mascot..jpg"
     // TODO: if user is loged in, get user data from backend to set the context
     changeUser(newUser);
 
@@ -64,22 +69,6 @@ function LoginPage() {
       console.log("Request complete! response:", res);
     });
     navigate('/');
-    // setCookie("google-token", response.tokenObj.id_token, {expires: new Date(response.tokenObj.expires_at), maxAge: response.tokenObj.expires_in});
-    
-    navigate('/');
-    /*
-    Set a cookie:
-    (if not exists (check on index page) --> navigate to login page, else stay there)
-    (when loging in: save token in cookie)
-
-    access_token: "ya29.A0ARrdaM_lMHPbloHuVEhfVCAzsM5OB6WiH-Es2PTZ0HMS6-0BiOSKzfF9rK60h7a52x-vfQ0Vo4N3k1YVD5ZYk99mk_2iAEwNqhFvZi0vZQpU8MAt5cLm6iqD5eOIkjtx5LaGJQZMGoFNY41NN9F_zu9tiqfu"
-    expires_at: 1651572228491
-    expires_in: 3599
-    first_issued_at: 1651568629491
-    id_token: "eyJhbGciOiJSUzI1NiIsImtpZCI6ImZjYmQ3ZjQ4MWE4MjVkMTEzZTBkMDNkZDk0ZTYwYjY5ZmYxNjY1YTIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMTAyODk3NTE4MDk1MS1nZWdpZzJudWNwNHR1ZnVtcHFtYzA0Z3Fob2Z1aWxyai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjEwMjg5NzUxODA5NTEtZ2VnaWcybnVjcDR0dWZ1bXBxbWMwNGdxaG9mdWlscmouYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDg1MjAxMDQ3OTk1ODE0NzAzNjAiLCJoZCI6InJvZ3VlbWFyay5jb20iLCJlbWFpbCI6ImtpbmdhQHJvZ3VlbWFyay5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6InlZQzREbzRhVzlXdWpleVFkUUtpYXciLCJuYW1lIjoiS2luZ2EgU2FsYSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHaWtmU3AweVpia3daZWd5OFl6ZmZITlluLVNrREc3cFJ2Rk5Gd209czk2LWMiLCJnaXZlbl9uYW1lIjoiS2luZ2EiLCJmYW1pbHlfbmFtZSI6IlNhbGEiLCJsb2NhbGUiOiJlbiIsImlhdCI6MTY1MTU2ODYyOSwiZXhwIjoxNjUxNTcyMjI5LCJqdGkiOiJkMTkyNDZhM2VmMjRmMTRjMzI1OWNhOGJjNWQ0MTU2NTI1MGY2Y2Y2In0.frAk8uUg8NNZMy-_Upju2ukqYCpdTWyVZp90EhnfmbDHn1OJrCKRl1VHzqlx8khc-3-gu5ShylSpePUmFggeu1eGxI5hl_ValtvO48y_77iwACkSJ4p90yaUhnuX5I5aBjmGibHG_mYpXmG9EUFwI-rupI2NcKbLeThta4-qNSmJjpBHarfqt12MVZx59llVxV2q8pgFhSpMcahZ_-r9D1qdphQ9L7HeQvKfDdrN6rbwPw7QWUYFLFI5_krajYgVOWxMMFB9qKopMwcf3_wT-dNN01nN7kR38igodov-MOWbwbA5fkcIvjsvPCaUWvej7R1jYvCcJ16JigAm5FDJaA"
-    idpId: "google"
-
-    */
   };
 
   if (isLoading) {
@@ -93,15 +82,6 @@ function LoginPage() {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
-
-  // useGoogleOneTapLogin({
-  //   onError: error => console.log(error),
-  //   onSuccess: response => console.log(response),
-  //   googleAccountConfigs: {
-  //     client_id: "826881705464-0nim7umsvtefh23q5pth5rgt4cdk4qge.apps.googleusercontent.com"
-  //   },
-  // });
-
 
   return (
 
@@ -117,14 +97,16 @@ function LoginPage() {
           data-login_uri="https://your.domain/your_login_endpoint"
           data-context="use">
       </div> */}
-          <GoogleLogin
+          {/* <GoogleLogin
             clientId="1028975180951-gegig2nucp4tufumpqmc04gqhofuilrj.apps.googleusercontent.com"
             buttonText="Login with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
             cookiePolicy={'single_host_origin'}
-          />
+          /> */}
+          {/* <GoogleOneTapLogin></GoogleOneTapLogin> */}
           {/* <GoogleOneTapLogin onError={(error) => console.log(error} onSuccess={(response) => console.log(response} googleAccountConfigs={{ client_id: "826881705464-0nim7umsvtefh23q5pth5rgt4cdk4qge.apps.googleusercontent.com" }} /> */}
+          <MicrosoftLogin clientId="20fdaf3b-cc20-4238-9fc0-183433e023ff" authCallback={responseGoogle} />
         </div>
       </div>
     </div>
