b5ec7ca42f58480839c44880eb735d786c3d5d4f
psyryuvok <dumitrescu_7_mihai@yahoo.com>
2022-06-04 18:45:25 +0300

    Add AI endpoint

--- src/pages/image-slide-show/ImageSlideShow.tsx
+++ src/pages/image-slide-show/ImageSlideShow.tsx
@@ -27,14 +27,13 @@ function ImageSlideShow() {
   const [lastDirection, setLastDirection] = useState('')
   const currentIndexRef = useRef(currentIndex)
   const [cookies, setCookie, removeCookie] = useCookies(['google-token', 'account-id']);
-
-  const images = [
-    'https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFieSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80',
-    'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__480.jpg',
-    'https://images.unsplash.com/photo-1583083527882-4bee9aba2eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80',
-    'https://i.pinimg.com/originals/7e/0a/50/7e0a507de8cf8b46e0f2665f1058ef37.jpg'
-  ];
-
+  const [images,setImages]=useState<any>()
+  // let images = [
+  //   'https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFieSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  //   'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__480.jpg',
+  //   'https://images.unsplash.com/photo-1583083527882-4bee9aba2eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80',
+  //   'https://i.pinimg.com/originals/7e/0a/50/7e0a507de8cf8b46e0f2665f1058ef37.jpg'
+  // ];
   const navigateTo = (path: string) => () => {
     return navigate(path);
   }
@@ -43,9 +42,27 @@ function ImageSlideShow() {
     if (!('google-token' in cookies)){
       navigate('/login');
     }
-  
+    fetch("http://localhost:8000/generate-images", {
+    method: "GET",
+    headers: {'Content-Type': 'application/json'},
+  })
+
     // To Test: console.log("cookies", cookies["google-token"]);
-    
+    fetch("http://localhost:8000/get-images", {
+    method: "GET",
+    headers: {'Content-Type': 'application/json'},
+  }).then( async res => {
+    let images_json = await res.json();
+    const image = []
+    for(var index in images_json["images"])
+    { 
+        image.push(images_json["images"][index]["path"])
+    }
+    setImages(image)
+  })
+
+
+    console.log(images)
     getImages();
     // OR
     // getRandom();
